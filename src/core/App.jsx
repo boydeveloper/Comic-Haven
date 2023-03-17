b3cf586d008c14dbaffc86206b745140b6cf078a
boy developer <daniel.onazi116@gmail.com>
2023-03-17 23:54:57 +0100

    Tweaks

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -3,7 +3,6 @@ import { Offline } from '../Pages/index';
 import { useEffect, useLayoutEffect } from 'react';
 function App() {
   const onLine = window.navigator.onLine;
-
   return onLine ? <RouterLinks /> : <Offline />;
 }
 
